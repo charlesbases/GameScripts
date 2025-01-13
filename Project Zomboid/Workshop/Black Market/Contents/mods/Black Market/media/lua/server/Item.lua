d2dc455e453ab6cc3068cbb1900e3d868e4a6e2f
zhiming.sun <zhiming.sun@qq.com>
2025-01-13 13:35:52 +0800

    add Project Zomboid

---
+++ Project Zomboid/Workshop/Black Market/Contents/mods/Black Market/media/lua/server/Item.lua
@@ -0,0 +1,92 @@
+-- money
+getScriptManager():getItem("Base.Money"):DoParam("Weight = 0")
+getScriptManager():getItem("Base.CreditCard"):DoParam("Weight = 0")
+
+-- 武士刀
+getScriptManager():getItem("Base.Katana"):DoParam("ConditionMax = 100") -- 耐久度
+getScriptManager():getItem("Base.Katana"):DoParam("ConditionLowerChanceOneIn = 100") -- 耐久损耗率(越高越好)
+
+-- M9 手枪
+getScriptManager():getItem("Base.Pistol"):DoParam("HitChance = 80") -- 命中率
+getScriptManager():getItem("Base.Pistol"):DoParam("AimingTime = 10") -- 瞄准时间
+getScriptManager():getItem("Base.Pistol"):DoParam("JamGunChance = 0") -- 卡壳率
+getScriptManager():getItem("Base.Pistol"):DoParam("ConditionMax = 100") -- 耐久度
+getScriptManager():getItem("Base.Pistol"):DoParam("ConditionLowerChanceOneIn = 100") -- 耐久损耗率(越高越好)
+-- M1911 手枪
+getScriptManager():getItem("Base.Pistol2"):DoParam("HitChance = 80")
+getScriptManager():getItem("Base.Pistol2"):DoParam("AimingTime = 10")
+getScriptManager():getItem("Base.Pistol2"):DoParam("JamGunChance = 0")
+getScriptManager():getItem("Base.Pistol2"):DoParam("ConditionMax = 100")
+getScriptManager():getItem("Base.Pistol2"):DoParam("ConditionLowerChanceOneIn = 100")
+-- 沙鹰手枪
+getScriptManager():getItem("Base.Pistol3"):DoParam("HitChance = 80")
+getScriptManager():getItem("Base.Pistol3"):DoParam("AimingTime = 10")
+getScriptManager():getItem("Base.Pistol3"):DoParam("JamGunChance = 0")
+getScriptManager():getItem("Base.Pistol3"):DoParam("ConditionMax = 100")
+getScriptManager():getItem("Base.Pistol3"):DoParam("ConditionLowerChanceOneIn = 100")
+-- M36 左轮手枪
+getScriptManager():getItem("Base.Revolver_Short"):DoParam("HitChance = 80")
+getScriptManager():getItem("Base.Revolver_Short"):DoParam("AimingTime = 10")
+getScriptManager():getItem("Base.Revolver_Short"):DoParam("JamGunChance = 0")
+getScriptManager():getItem("Base.Revolver_Short"):DoParam("ConditionMax = 100")
+getScriptManager():getItem("Base.Revolver_Short"):DoParam("ConditionLowerChanceOneIn = 100")
+-- M625 左轮手枪
+getScriptManager():getItem("Base.Revolver"):DoParam("HitChance = 80")
+getScriptManager():getItem("Base.Revolver"):DoParam("AimingTime = 10")
+getScriptManager():getItem("Base.Revolver"):DoParam("JamGunChance = 0")
+getScriptManager():getItem("Base.Revolver"):DoParam("ConditionMax = 100")
+getScriptManager():getItem("Base.Revolver"):DoParam("ConditionLowerChanceOneIn = 100")
+-- 马格南左轮手枪
+getScriptManager():getItem("Base.Revolver_Long"):DoParam("HitChance = 80")
+getScriptManager():getItem("Base.Revolver_Long"):DoParam("AimingTime = 10")
+getScriptManager():getItem("Base.Revolver_Long"):DoParam("JamGunChance = 0")
+getScriptManager():getItem("Base.Revolver_Long"):DoParam("ConditionMax = 100")
+getScriptManager():getItem("Base.Revolver_Long"):DoParam("ConditionLowerChanceOneIn = 100")
+-- MSR700 猎狐步枪
+getScriptManager():getItem("Base.VarmintRifle"):DoParam("HitChance = 80")
+getScriptManager():getItem("Base.VarmintRifle"):DoParam("AimingTime = 10")
+getScriptManager():getItem("Base.VarmintRifle"):DoParam("JamGunChance = 0")
+getScriptManager():getItem("Base.VarmintRifle"):DoParam("ConditionMax = 100")
+getScriptManager():getItem("Base.VarmintRifle"):DoParam("ConditionLowerChanceOneIn = 100")
+-- MSR788 狩猎步枪
+getScriptManager():getItem("Base.HuntingRifle"):DoParam("HitChance = 80")
+getScriptManager():getItem("Base.HuntingRifle"):DoParam("AimingTime = 10")
+getScriptManager():getItem("Base.HuntingRifle"):DoParam("JamGunChance = 0")
+getScriptManager():getItem("Base.HuntingRifle"):DoParam("ConditionMax = 100")
+getScriptManager():getItem("Base.HuntingRifle"):DoParam("ConditionLowerChanceOneIn = 100")
+-- M14 自动步枪
+getScriptManager():getItem("Base.AssaultRifle2"):DoParam("HitChance = 80")
+getScriptManager():getItem("Base.AssaultRifle2"):DoParam("AimingTime = 10")
+getScriptManager():getItem("Base.AssaultRifle2"):DoParam("JamGunChance = 0")
+getScriptManager():getItem("Base.AssaultRifle2"):DoParam("ConditionMax = 100")
+getScriptManager():getItem("Base.AssaultRifle2"):DoParam("ConditionLowerChanceOneIn = 100")
+-- M16 突击步枪
+getScriptManager():getItem("Base.AssaultRifle"):DoParam("HitChance = 80")
+getScriptManager():getItem("Base.AssaultRifle"):DoParam("AimingTime = 10")
+getScriptManager():getItem("Base.AssaultRifle"):DoParam("JamGunChance = 0")
+getScriptManager():getItem("Base.AssaultRifle"):DoParam("ConditionMax = 100")
+getScriptManager():getItem("Base.AssaultRifle"):DoParam("ConditionLowerChanceOneIn = 100")
+-- JS-2000 霰弹枪
+getScriptManager():getItem("Base.Shotgun"):DoParam("HitChance = 80")
+getScriptManager():getItem("Base.Shotgun"):DoParam("AimingTime = 10")
+getScriptManager():getItem("Base.Shotgun"):DoParam("JamGunChance = 0")
+getScriptManager():getItem("Base.Shotgun"):DoParam("ConditionMax = 100")
+getScriptManager():getItem("Base.Shotgun"):DoParam("ConditionLowerChanceOneIn = 100")
+-- JS-2000 霰弹枪(锯短)
+getScriptManager():getItem("Base.ShotgunSawnoff"):DoParam("HitChance = 80")
+getScriptManager():getItem("Base.ShotgunSawnoff"):DoParam("AimingTime = 10")
+getScriptManager():getItem("Base.ShotgunSawnoff"):DoParam("JamGunChance = 0")
+getScriptManager():getItem("Base.ShotgunSawnoff"):DoParam("ConditionMax = 100")
+getScriptManager():getItem("Base.ShotgunSawnoff"):DoParam("ConditionLowerChanceOneIn = 100")
+-- 双管霰弹枪
+getScriptManager():getItem("Base.DoubleBarrelShotgun"):DoParam("HitChance = 80")
+getScriptManager():getItem("Base.DoubleBarrelShotgun"):DoParam("AimingTime = 10")
+getScriptManager():getItem("Base.DoubleBarrelShotgun"):DoParam("JamGunChance = 0")
+getScriptManager():getItem("Base.DoubleBarrelShotgun"):DoParam("ConditionMax = 100")
+getScriptManager():getItem("Base.DoubleBarrelShotgun"):DoParam("ConditionLowerChanceOneIn = 100")
+-- 双管霰弹枪(锯短)
+getScriptManager():getItem("Base.DoubleBarrelShotgunSawnoff"):DoParam("HitChance = 80")
+getScriptManager():getItem("Base.DoubleBarrelShotgunSawnoff"):DoParam("AimingTime = 10")
+getScriptManager():getItem("Base.DoubleBarrelShotgunSawnoff"):DoParam("JamGunChance = 0")
+getScriptManager():getItem("Base.DoubleBarrelShotgunSawnoff"):DoParam("ConditionMax = 100")
+getScriptManager():getItem("Base.DoubleBarrelShotgunSawnoff"):DoParam("ConditionLowerChanceOneIn = 100")
